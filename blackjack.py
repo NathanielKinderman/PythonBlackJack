df5a99085e3696ea65f26f0ad4055fd14ac45e19
natek <natekinderman16@gmail.com>
2020-04-22 13:37:55 -0500

    Created class Hand to act like the players hand. SEt the attibutes cards, value , aces for hand object. Added function add_card(), to append the card object to the hand cards dictionary and added the value from card object to hand value. Add function for adjusting_for_ace but will add logic to that at later time. Tested the class Deck and Hand and the functions deck.shuffle and hand.add_card. They work and no errors occured

--- blackjack.py
+++ blackjack.py
@@ -45,5 +45,30 @@ class Deck:
         single_card = self.deck.pop()
         return single_card
 
+#class hand will equal player
+class Hand:
+    #will need attributes: card , value and aces(for the ace special rule)
+    #self.card is wll need to be an empty list like deck
+    #self.value will need to start at 0
+    #self.aces will need to keep track of aces in deck
+    def __init__(self):        
+        self.cards = []
+        self.value = 0
+        self.aces = 0
+
+    def add_card(self,card):
+        self.cards.append(card)
+        self.value += values[card.rank]
+
+    def adjust_for_ace(self):
+        pass
+
 test_deck = Deck()
-print(test_deck)
+test_deck.shuffle()
+test_player = Hand()
+test_player.add_card(test_deck.deal())
+test_player.add_card(test_deck.deal())
+test_player.value
+
+for card in test_player.cards:
+    print(card)
